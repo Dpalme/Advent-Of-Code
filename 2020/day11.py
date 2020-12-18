92ffad5ec8d9ab7926484886446ad33a53617c4b
Dpalme <diegoPalmerin24@gmail.com>
2020-12-18 06:49:06 -0600

    code recycling

--- 2020/day11.py
+++ 2020/day11.py
@@ -1,63 +1,51 @@
-def cycle_fp(seats):
-    copied = [[val for val in row] for row in seats]
-    xmax, ymax = len(seats[0]), len(seats)
-    dirs = [(-1, -1), (-1, 0), (-1, 1), (0, -1),
-            (0, 1), (1, -1), (1, 0), (1, 1)]
-    for y, row in enumerate(seats):
-        for x, seat in enumerate(row):
-            crowded = 0
-            for i, j in dirs:
-                if 0 <= x + i and x + i < xmax and 0 <= y + j and y + j < ymax:
-                    crowded += 1 if seats[y + j][x + i] == '#' else 0
+from itertools import product
+from functools import lru_cache
 
+dirs = set(product((-1, 0, 1), repeat=2)).difference(set([(0, 0)]))
+
+
+def crowd(x, y, xm, ym, sts, line):
+    if line:
+        crowded = 0
+        for i, j in dirs:
+            dx, dy = x + i, y + j
+            while 0 <= dx and dx < xm and 0 <= dy and dy < ym:
+                if sts[dy][dx] != '.':
+                    crowded += 1 if sts[dy][dx] == '#' else 0
+                    break
+                dx, dy = dx + i, dy + j
+        return crowded
+    else:
+        return sum([sts[y+j][x+i] == '#' for i, j in dirs
+                    if 0 <= x+i and x+i < xm and 0 <= y+j and y+j < ym])
+
+
+def cycle(sts, xm, ym, line):
+    copied = [[val for val in row] for row in sts]
+    for y in range(ym):
+        for x, seat in enumerate(sts[y]):
+            crowded = crowd(x, y, xm, ym, sts, line)
             if seat == 'L' and crowded == 0:
                 copied[y][x] = '#'
-            elif seat == '#' and crowded > 3:
+            elif seat == '#' and crowded > 3 + line:
                 copied[y][x] = 'L'
     return copied
 
 
-def first_part(input_data):
-    seats = [[char for char in line] for line in input_data.split('\n')]
-    res = None
-    while res != seats:
-        res = seats
-        seats = cycle_fp(seats)
-    return sum([sum([1 for val in row if val == '#']) for row in seats])
+def setup(inp_str, line):
+    sts = [[st for st in ln] for ln in inp_str.split('\n')]
+    xm, ym, res = len(sts[0]), len(sts), None
+    while res != sts:
+        res, sts = sts, cycle(sts, xm, ym, line)
+    return sum([sum([val == '#' for val in row]) for row in sts])
 
 
-def cycle_sp(seats):
-    copied = [[val for val in row] for row in seats]
-    xmax, ymax = len(seats[0]), len(seats)
-    dirs = [(-1, -1), (-1, 0), (-1, 1), (0, -1),
-            (0, 1), (1, -1), (1, 0), (1, 1)]
-    for y in range(ymax):
-        for x, seat in enumerate(seats[y]):
-            crowded = 0
-            for i, j in dirs:
-                dx, dy = x + i, y + j
-                while 0 <= dx and dx < xmax and 0 <= dy and dy < ymax:
-                    if seats[dy][dx] != '.':
-                        crowded += 1 if seats[dy][dx] == '#' else 0
-                        break
-                    dx, dy = dx + i, dy + j
-
-            if seat == 'L' and crowded == 0:
-                copied[y][x] = '#'
-            elif seat == '#' and crowded > 4:
-                copied[y][x] = 'L'
-    return copied
+def first_part(inp_str):
+    return setup(inp_str, False)
 
 
-def second_part(input_data):
-    seats = [[char for char in line] for line in input_data.split('\n')]
-    res = None
-    cont = 0
-    while res != seats:
-        cont += 1
-        res = seats
-        seats = cycle_sp(seats)
-    return sum([sum([1 for val in row if val == '#']) for row in seats])
+def second_part(inp_str):
+    return setup(inp_str, True)
 
 
 if __name__ == '__main__':
