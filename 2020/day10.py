37bf8abf89712cfb23fcd60651bfdf70fa1cfe3e
Dpalme <diegoPalmerin24@gmail.com>
2020-12-18 05:14:06 -0600

    optimization

--- 2020/day10.py
+++ 2020/day10.py
@@ -1,23 +1,20 @@
-def first_part(input_data):
-    adapts = set([int(nbr) for nbr in input_data.split('\n')])
-    adds = [0, 0, 0, 1]
-    for adap in adapts:
-        adds[adap - adds[0]] += 1
-        adds[0] = adap
-    return adds[1] * adds[3]
+def first_part(inp_str):
+    arr, acs = set([int(nbr) for nbr in inp_str.split('\n')]), [0, 0, 0, 1]
+    for ad in arr:
+        acs[ad - acs[0]], acs[0] = acs[ad - acs[0]] + 1, ad
+    return acs[1] * acs[3]
 
 
-def second_part(input_data):
-    adapts = sorted([0] + [int(nbr) for nbr in input_data.split('\n')])
-    acum = [1] + [0 for i in range(len(adapts) - 1)]
-    for ind in range(len(adapts)):
-        acum[ind] += sum([acum[ind2] for ind2, imp in enumerate(adapts[:ind])
-                          if adapts[ind] - adapts[ind2] <= 3])
-    return acum[-1]
+def second_part(inp_str):
+    arr = sorted([0] + [int(nbr) for nbr in inp_str.split('\n')])
+    acs = [1] + [0 for i in range(len(arr) - 1)]
+    for x, ax in enumerate(arr):
+        acs[x] += sum([acs[y] for y in range(x-3, x) if ax - arr[y] <= 3])
+    return acs[-1]
 
 
 if __name__ == '__main__':
     with open('2020/inputs/day10.txt', 'r') as inp:
-        input_string = inp.read()
-        print('First part: %d' % first_part(input_string))
-        print('Second part: %d' % second_part(input_string))
+        inp_str = inp.read()
+        print('First part: %d' % first_part(inp_str))
+        print('Second part: %d' % second_part(inp_str))
