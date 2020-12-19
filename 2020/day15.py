22e18015008e614a3e42ea013d4697395337e178
Dpalme <diegoPalmerin24@gmail.com>
2020-12-19 16:12:33 -0600

    code recycling

--- 2020/day15.py
+++ 2020/day15.py
@@ -1,25 +1,15 @@
 from sys import stdout
 
 
-def first_part(input_data):
-    spoke = {int(numb): time + 1 for time,
-             numb in enumerate(input_data.split(','))}
+def play(inp_str, stop):
+    spoke = {int(numb): time + 1 for time, numb in enumerate(inp_str.split(','))}
     numb = list(spoke.keys())[-1]
-    for i in range(len(spoke), 2020):
-        spoke[numb], numb = (i, i - spoke[numb]) if numb in spoke else (i, 0)
-    return numb
-
-
-def second_part(input_data):
-    spoke = {int(numb): time + 1 for time,
-             numb in enumerate(input_data.split(','))}
-    numb = list(spoke.keys())[-1]
-    for i in range(len(spoke), 30000000):
+    for i in range(len(spoke), stop):
         spoke[numb], numb = (i, i - spoke[numb]) if numb in spoke else (i, 0)
     return numb
 
 
 with open('2020/inputs/day15.txt', 'r') as inp:
     inp_str = inp.read()
-    stdout.write(f'Day 15\nFirst part: {first_part(inp_str)}\n')
-    stdout.write(f'Second part: {second_part(inp_str)}\n')
+    stdout.write(f'Day 15\nFirst part: {play(inp_str, 2020)}\n')
+    stdout.write(f'Second part: {play(inp_str, 30000000)}\n')
