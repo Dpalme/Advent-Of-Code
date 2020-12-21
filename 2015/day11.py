c16207963b1ed699557a463079553655457d0e16
Dpalme <diegoPalmerin24@gmail.com>
2020-12-21 01:15:47 -0600

    day 11 2015

--- 2015/day11.py
+++ 2015/day11.py
@@ -1,44 +1,32 @@
-input_string = 'vzbxkghb'
+inp_str = 'vzbxkghb'
 
 
-def debg(numbers):
-    print(numbers)
-    for ind in range(len(numbers[:-2])):
-        print(numbers[:-2])
-        print(numbers[ind] == numbers[ind + 1], numbers[ind + 1] == numbers[ind + 2])
-
-
-def check_valid(numbers):
-    letters = 8 not in numbers * 14 not in numbers * 11 not in numbers
-    straight, pair = 1, 0
-    for ind in range(len(numbers[:-2])):
-        straight *= 0 if (numbers[ind] + 1 == numbers[ind + 1]) * (
-            numbers[ind] + 2 == numbers[ind + 2]) else 1
-        pair += (numbers[ind] == numbers[ind + 1]) + \
-            (numbers[ind + 1] == numbers[ind + 2])
+def check_valid(nmbrs):
+    letters = (8 not in nmbrs) * (14 not in nmbrs) * (11 not in nmbrs)
+    straight, pair, bind = 1, 0, 8
+    for i, num in enumerate(nmbrs[:-1]):
+        n1 = nmbrs[i+1]
+        if i != 6:
+            straight *= 1 - ((num+1 == n1) * (num+2 == nmbrs[i+2]))
+        if i != bind:
+            if num == n1:
+                bind, pair = i+1, pair+1
     return letters * (not straight) * (pair >= 2)
 
 
-def first_part(input_data=input_string):
-    numbers = [(ord(char) - 97) for char in input_data]
-    print(numbers)
-    numbers[-1] += 1
-    while not check_valid(numbers):
-        numbers[-1] += 1
-        for ind in reversed(range(len(numbers))):
-            if numbers[ind] > 25:
-                numbers[ind - 1] += 1
-                numbers[ind] = 0
-    debg(numbers)
-    return "".join([chr(num + 97) for num in numbers])
-
-
-def second_part(input_data=input_string):
-    for char in input_data:
-        pass
-    return 0
+def nxt_password(psswrd):
+    nmbrs = [(ord(char) - 97) for char in psswrd]
+    nmbrs[7] += 1
+    while not check_valid(nmbrs):
+        nmbrs[7] += 1
+        if nmbrs[7] > 25:
+            for i, num in enumerate(reversed(nmbrs)):
+                if num > 25:
+                    nmbrs[7-i], nmbrs[6-i] = 0, nmbrs[6-i]+1
+    return "".join([chr(num + 97) for num in nmbrs])
 
 
 if __name__ == '__main__':
-    print('First part: %s' % first_part('ghijklmn'))
-    print('Second part: %s' % second_part())
+    f_password = nxt_password(inp_str)
+    print(f'First part: {f_password}')
+    print(f'Second part: {nxt_password(f_password)}')
