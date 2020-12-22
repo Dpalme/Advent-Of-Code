c3ce35c3d4d06ae335349d3021b8f041427d9d07
Dpalme <diegoPalmerin24@gmail.com>
2020-12-22 07:12:17 -0600

    cache optimization

--- 2020/day7.py
+++ 2020/day7.py
@@ -3,41 +3,54 @@ from sys import stdout
 from functools import lru_cache
 
 with open('2020/inputs/day7.txt', 'r') as inp:
-    inp_str = inp.read()
+    inp_str = inp.read().split('\n')
+
+
+@lru_cache(1)
+def get_regex():
+    res = set()
+    for line in inp_str:
+        rs = search(r'(.*)\scontain\s(.*)', line[:-1])
+        res.add((rs.group(1), rs.group(2)))
+    return res
 
 
 @lru_cache(None)
 def get_directs(bag):
     direct = []
-    for line in inp_str.split('\n'):
-        res = search(r'(.*)\scontain\s(.*)', line[:-1])
-        if bag in res.group(2) or bag[:-1] in res.group(2):
-            direct.append(res.group(1))
+    for key, cont in get_regex():
+        if bag in cont or bag[:-1] in cont:
+            direct.append(key)
     return list(set(direct))
 
 
 def first_part():
     direct = get_directs('shiny gold bag')
-    for base in direct:
-        direct.extend(get_directs(base))
+    [direct.extend(get_directs(base)) for base in direct]
     return len(set(direct))
 
 
-def count_bags(key, bags):
-    key += "s" if key[-1] != 's' else ""
-    smt = (sum([bnum + bnum * count_bags(bag, bags)
-                for bag, bnum in bags[key].items()])
-           if type(bags[key]) == dict else 0)
-    return smt
+@lru_cache(1)
+def get_bags():
+    bags = {}
+    for key, cont in get_regex():
+        bags[key] = {bag[2:]: int(bag[0]) for bag in cont.split(
+            ', ')} if 'no' not in cont else 0
+    return bags
+
+
+@lru_cache(None)
+def count_bags(key):
+    if key[-1] != 's':
+        key += 's'
+    bags = get_bags()
+    return (sum(bnum + bnum * count_bags(bag)
+                for bag, bnum in bags[key].items())
+            if isinstance(bags[key], dict) else 0)
 
 
 def second_part(inp_str):
-    bags = {}
-    for line in inp_str.split('\n'):
-        res = search(r'(.*)\scontain\s(.*)', line[:-1])
-        bags[res.group(1)] = {bag[2:]: int(bag[0]) for bag in res.group(
-            2).split(', ')} if 'no' not in res.group(2) else 0
-    return count_bags('shiny gold bags', bags)
+    return count_bags('shiny gold bags')
 
 
 stdout.write(f'Day 7\nFirst part: {first_part()}\n')
