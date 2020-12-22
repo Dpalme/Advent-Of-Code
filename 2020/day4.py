11a5daf8d3bdb641d959127719c1e58a08431129
Dpalme <diegoPalmerin24@gmail.com>
2020-12-22 07:47:24 -0600

    iteration optimization

--- 2020/day4.py
+++ 2020/day4.py
@@ -2,41 +2,36 @@ from sys import stdout
 
 
 def inr(mn, mx, val):
+    val = int(val)
     return mn <= val and mx >= val
 
 
 def first_part(inp_str):
-    pss = {}
     psss = []
-    for line in inp_str.split("\n"):
-        if line != "":
-            for propert in [propert.split(':') for propert in line.split()]:
-                pss[propert[0]] = propert[1]
-        else:
-            if ("byr" in pss and "iyr" in pss and
-                "eyr" in pss and "hgt" in pss and
-                "hcl" in pss and "ecl" in pss and
-                    "pid" in pss):
-                psss.append(pss)
-            pss = {}
+    for ln in inp_str.split("\n\n"):
+        pss = {p[0]: p[1]
+               for p in [pr.split(':')
+                         for pr in ln.replace('\n', ' ').split()]}
+        if ("byr" in pss and "iyr" in pss and
+            "eyr" in pss and "hgt" in pss and
+            "hcl" in pss and "ecl" in pss and
+                "pid" in pss):
+            psss.append(pss)
     return psss
 
 
 def second_part(psss):
-    valid = 0
-    for pss in psss:
-        valid += (inr(1920, 2002, int(pss["byr"])) *
-                  inr(2010, 2020, int(pss["iyr"])) *
-                  inr(2020, 2030, int(pss["eyr"])) *
-                  (("cm" in pss["hgt"] and
-                    inr(150, 193, int(pss["hgt"][:-2]))) or
-                   ("in" in pss["hgt"] and
-                    inr(59, 76, int(pss["hgt"][:-2])))) *
-                  ("#" in pss["hcl"] and len(pss["hcl"]) == 7) *
-                  pss["ecl"] in ["amb", "blu", "brn", "gry",
-                                 "grn", "hzl", "oth"] and
-                  len(pss["pid"]) == 9)
-    return valid
+    return sum((inr(1920, 2002, pss["byr"]) *
+                inr(2010, 2020, pss["iyr"]) *
+                inr(2020, 2030, pss["eyr"]) *
+                (("cm" in pss["hgt"] and
+                    inr(150, 193, pss["hgt"][:-2])) or
+                 ("in" in pss["hgt"] and
+                    inr(59, 76, pss["hgt"][:-2]))) *
+                ("#" in pss["hcl"] and len(pss["hcl"]) == 7) *
+                pss["ecl"] in ("amb", "blu", "brn", "gry",
+                               "grn", "hzl", "oth") and
+                len(pss["pid"]) == 9) for pss in psss)
 
 
 with open('2020/inputs/day4.txt', 'r') as inp:
