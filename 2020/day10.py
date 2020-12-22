12261fc023d43969e91ef84d35e852576389312e
Dpalme <diegoPalmerin24@gmail.com>
2020-12-22 06:03:33 -0600

    sum optimization

--- 2020/day10.py
+++ 2020/day10.py
@@ -2,7 +2,7 @@ from sys import stdout
 
 
 def first_part(inp_str):
-    arr, acs = set([int(nbr) for nbr in inp_str.split('\n')]), [0, 0, 0, 1]
+    arr, acs = set(int(nbr) for nbr in inp_str.split('\n')), [0, 0, 0, 1]
     for ad in arr:
         acs[ad - acs[0]], acs[0] = acs[ad - acs[0]] + 1, ad
     return acs[1] * acs[3]
@@ -12,7 +12,7 @@ def second_part(inp_str):
     arr = sorted([0] + [int(nbr) for nbr in inp_str.split('\n')])
     acs = [1] + [0 for i in range(len(arr) - 1)]
     for x, ax in enumerate(arr):
-        acs[x] += sum([acs[y] for y in range(x-3, x) if ax - arr[y] <= 3])
+        acs[x] += sum(acs[y] for y in range(x-3, x) if ax-arr[y] <= 3)
     return acs[-1]
 
 
