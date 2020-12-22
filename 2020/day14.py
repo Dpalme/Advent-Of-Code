0be7e235e064615c492380d96ba2dfea9aaf04b9
Dpalme <diegoPalmerin24@gmail.com>
2020-12-22 08:01:42 -0600

    tuple input parsing optimization

--- 2020/day14.py
+++ 2020/day14.py
@@ -4,15 +4,14 @@ from sys import stdout
 
 def first_part(lines):
     mem, mask = {}, None
-    for line in lines:
-        res = line.split()
-        if 'mem' in line:
-            adrs, val = res[0][4:-1], bin(int(res[2]))[2:]
+    for cmd, _, val in lines:
+        if 'mem' in cmd:
+            adrs, val = cmd[4:-1], bin(int(val))[2:]
             val = "0" * (len(mask) - len(val)) + val
             mem[adrs] = int("".join(mask[ind] if itm != "X" else val[ind]
-                                     for ind, itm in enumerate(mask)), base=2)
+                                    for ind, itm in enumerate(mask)), base=2)
         else:
-            mask = tuple(x for x in res[2])
+            mask = tuple(x for x in val)
     return sum(mem.values())
 
 
@@ -29,20 +28,19 @@ def iterations(res):
 
 def second_part(lines):
     mem, mask = {}, None
-    for line in lines:
-        res = line.split()
-        if 'mem' in line:
-            adrs, val = bin(int(res[0][4:-1]))[2:], int(res[2])
+    for cmd, _, val in lines:
+        if 'mem' in cmd:
+            adrs, val = bin(int(cmd[4:-1]))[2:], int(val)
             adrs = "0" * (len(mask) - len(adrs)) + adrs
             res = [adrs[i] * (mask[i] == "0") + mask[i] * (mask[i] != "0")
                    for i in range(len(mask))]
             mem.update({address: val for address in iterations([res])})
         else:
-            mask = tuple(x for x in res[2])
+            mask = tuple(x for x in val)
     return sum(mem.values())
 
 
 with open('2020/inputs/day14.txt', 'r') as inp:
-    inp_str = inp.readlines()
+    inp_str = [tuple(ln.split()) for ln in inp.readlines()]
     stdout.write(f'Day 14\nFirst part: {first_part(inp_str)}\n')
     stdout.write(f'Second part: {second_part(inp_str)}\n')
