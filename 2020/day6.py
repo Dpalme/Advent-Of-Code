b1492bc6b66af22a51642597477252cf5ab848d4
Dpalme <diegoPalmerin24@gmail.com>
2020-12-22 07:24:13 -0600

    set optimization

--- 2020/day6.py
+++ 2020/day6.py
@@ -2,18 +2,16 @@ from sys import stdout
 
 
 def first_part(inp_str):
-    return sum([len(set([lttr for lttr in form.replace('\n', '')]))
-               for form in inp_str.split('\n\n')])
+    return sum(len(set((le for le in f.replace('\n', '')))) for f in inp_str)
 
 
-def second_part(inp_str, total=[]):
-    for forms in inp_str.split('\n\n'):
-        total.append([set(form) for form in forms.split('\n')])
-        [total[-1][0].intersection_update(form) for form in total[-1][1:]]
-    return sum([len(form[0]) for form in total])
+def second_part(inp_str):
+    return sum(len(set.intersection(*(set(f)
+                                      for f in fs.split('\n'))))
+               for fs in inp_str)
 
 
 with open('2020/inputs/day6.txt', 'r') as inp:
-    inp_str = inp.read()
+    inp_str = inp.read().split('\n\n')
     stdout.write(f'Day 6\nFirst part: {first_part(inp_str)}\n')
     stdout.write(f'Second part: {second_part(inp_str)}\n')
