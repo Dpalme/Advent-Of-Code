8b1aefa41a79f4cbb52400c604dec9ca1590c381
Dpalme <diegoPalmerin24@gmail.com>
2020-12-22 11:58:15 -0600

    now only runs once instead of starting over

--- 2020/day15.py
+++ 2020/day15.py
@@ -1,16 +1,20 @@
 from sys import stdout
 
 
-def play(strt, stop):
+def play(strt, stop1, stop):
     spoke, numb = [0] * stop, strt[-1]
     for time, numb in enumerate(strt):
         spoke[numb] = time + 1
-    for i in range(len(strt), stop):
+    for i in range(len(strt), stop1):
         spoke[numb], numb = i, i - spoke[numb] if spoke[numb] != 0 else 0
-    return numb
+    back = numb
+    for i in range(stop1, stop):
+        spoke[numb], numb = i, i - spoke[numb] if spoke[numb] != 0 else 0
+    return back, numb
 
 
 with open('2020/inputs/day15.txt', 'r') as inp:
     strt = tuple(int(x) for x in inp.read().split(','))
-    stdout.write(f'Day 15\nFirst part: {play(strt, 2020)}\n')
-    stdout.write(f'Second part: {play(strt, 30000000)}\n')
+    p1, p2 = play(strt, 2020, 30000000)
+    stdout.write(f'Day 15\nFirst part: {p1}\n')
+    stdout.write(f'Second part: {p2}\n')
