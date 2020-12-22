fa1f72f032cdff6a2dac4a802060f9c2cfc9f4b4
Dpalme <diegoPalmerin24@gmail.com>
2020-12-22 07:51:51 -0600

    input parsing optimization

--- 2020/day18.py
+++ 2020/day18.py
@@ -18,14 +18,15 @@ def evaluate(exp):
 
 
 def first_part(inp_str):
-    return sum(evaluate(ln) for ln in inp_str.split('\n'))
+    return sum(evaluate(ln) for ln in lns)
 
 
 def second_part(inp_str):
-    return sum(evaluate(sub(r'[+]', '*', ln)) for ln in inp_str.split('\n'))
+    return sum(evaluate(sub(r'[+]', '*', ln)) for ln in lns)
 
 
 with open('2020/inputs/day18.txt', 'r') as inp:
     inp_str = sub(r'[*]', '-', inp.read())
-    stdout.write(f'Day 18\nFirst part: {first_part(inp_str)}\n')
-    stdout.write(f'Second part: {second_part(inp_str)}\n')
+    lns = inp_str.split('\n')
+    stdout.write(f'Day 18\nFirst part: {first_part(lns)}\n')
+    stdout.write(f'Second part: {second_part(lns)}\n')
