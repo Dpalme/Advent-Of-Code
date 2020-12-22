126a3e4b9c76bd3e3b50bbd36f851694014b1e47
Dpalme <diegoPalmerin24@gmail.com>
2020-12-22 08:11:17 -0600

    import and first part optimization

--- 2020/day2.py
+++ 2020/day2.py
@@ -1,29 +1,29 @@
-import re
+from re import search, findall
 from sys import stdout
 
 
 def first_part(input_data):
     valid = 0
-    for line in input_data.split("\n"):
-        res = re.search(r'(\d*?)-(\d*)\s(\w):\s(.*)', line)
-        low, upp = int(res.group(1)), int(res.group(2))
-        value = len(re.findall(res.group(3), res.group(4)))
-        valid += (low <= value) and (value <= upp)
+    for line in input_data:
+        rs = search(r'(\d*?)-(\d*)\s(\w):\s(.*)', line)
+        valid += (int(rs.group(1)) <=
+                  len(findall(rs.group(3), rs.group(4))) <=
+                  int(rs.group(2)))
     return valid
 
 
 def second_part(input_data):
     valid = 0
-    for line in input_data.split("\n"):
-        res = re.search(r'(\d*?)-(\d*)\s(\D):\s(\D*)', line)
-        pos1, pos2, letter, password = int(res.group(1)) - 1, int(
-            res.group(2)) - 1, res.group(3), res.group(4)
+    for line in input_data:
+        rs = search(r'(\d*?)-(\d*)\s(\D):\s(\D*)', line)
+        pos1, pos2, letter, password = int(rs.group(1)) - 1, int(
+            rs.group(2)) - 1, rs.group(3), rs.group(4)
         valid += (password[pos1] == letter) * (password[pos2] != letter) + \
             (password[pos2] == letter) * (password[pos1] != letter)
     return valid
 
 
 with open('2020/inputs/day2.txt', 'r') as inp:
-    inp_str = inp.read()
+    inp_str = inp.readlines()
     stdout.write(f'Day 2\nFirst part: {first_part(inp_str)}\n')
     stdout.write(f'Second part: {second_part(inp_str)}\n')
