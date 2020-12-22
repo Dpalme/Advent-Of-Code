7f416f76bcd0c7e232574d3e1d4fea5ea9b76f09
Dpalme <diegoPalmerin24@gmail.com>
2020-12-22 08:13:06 -0600

    operation optimization

--- 2020/day1.py
+++ 2020/day1.py
@@ -4,17 +4,19 @@ from sys import stdout
 
 def first_part(numbers):
     for n in numbers:
-        if 2020 - n in numbers:
-            return n * (2020 - n)
+        i = 2020 - n
+        if i in numbers:
+            return n * i
 
 
 def second_part(numbers):
     for n1, n2 in combinations(numbers, 2):
-        if 2020 - n1 - n2 in numbers:
-            return (2020 - n1 - n2) * n1 * n2
+        i = 2020 - n1 - n2
+        if i in numbers:
+            return i * n1 * n2
 
 
 with open('2020/inputs/day1.txt', 'r') as inp:
-    numbers = set([int(x) for x in inp.readlines()])
+    numbers = set((int(x) for x in inp.readlines()))
     stdout.write(f'Day 1\nFirst part: {first_part(numbers)}\n')
     stdout.write(f'Second part: {second_part(numbers)}\n')
