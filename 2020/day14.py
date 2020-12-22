934ed8a4725c1a4635cf52689ac3c7fbf73f7d89
Dpalme <diegoPalmerin24@gmail.com>
2020-12-22 05:35:23 -0600

    tuple optimization

--- 2020/day14.py
+++ 2020/day14.py
@@ -3,16 +3,16 @@ from sys import stdout
 
 
 def first_part(input_data):
-    lines, mem, mask = input_data.split('\n'), {}, []
+    lines, mem, mask = input_data.split('\n'), {}, None
     for line in lines:
         res = line.split()
         if 'mem' in line:
             adrs, val = res[0][4:-1], bin(int(res[2]))[2:]
             val = "0" * (len(mask) - len(val)) + val
-            mem[adrs] = int("".join([mask[ind] if itm != "X" else val[ind]
-                                     for ind, itm in enumerate(mask)]), base=2)
+            mem[adrs] = int("".join(mask[ind] if itm != "X" else val[ind]
+                                     for ind, itm in enumerate(mask)), base=2)
         else:
-            mask = [x for x in res[2]]
+            mask = tuple(x for x in res[2])
     return sum(mem.values())
 
 
@@ -24,22 +24,22 @@ def iterations(res):
             zero[ind], one[ind] = '0', '1'
             res.remove(entry)
             res.extend([one, zero])
-    return [int("".join(entry), base=2) for entry in res]
+    return tuple(int("".join(entry), base=2) for entry in res)
 
 
 def second_part(input_data):
-    mem, mask, lines = {}, [], input_data.split('\n')
+    mem, mask, lines = {}, None, input_data.split('\n')
     for line in lines:
         res = line.split()
         if 'mem' in line:
             adrs, val = bin(int(res[0][4:-1]))[2:], int(res[2])
             adrs = "0" * (len(mask) - len(adrs)) + adrs
-            res = [adrs[ind] if mask[ind] == "0" else mask[ind]
-                   for ind in range(len(mask))]
+            res = [adrs[i] * (mask[i] == "0") + mask[i] * (mask[i] != "0")
+                   for i in range(len(mask))]
             mem.update({address: val for address in iterations([res])})
         else:
-            mask = [x for x in res[2]]
-    return sum([val for val in mem.values()])
+            mask = tuple(x for x in res[2])
+    return sum(mem.values())
 
 
 with open('2020/inputs/day14.txt', 'r') as inp:
