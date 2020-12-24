06b35c25cc64ef00c72b316c5606189ac7814446
Dpalme <diegoPalmerin24@gmail.com>
2020-12-24 00:55:47 -0600

    optimizations

--- 2020/day24.py
+++ 2020/day24.py
@@ -9,32 +9,27 @@ dir_tup = tuple(dirs.values())
 def first_part(inp_str):
     tiles = {}
     for line in inp_str:
-        line = sub('se', 'u', sub('sw', 'b', sub(
-            'ne', 'v', sub('nw', 'p', line))))
         pos = (0, 0)
         for d in line:
             pos = tuple(map(sum, zip(pos, dirs[d])))
         if pos in tiles:
             del tiles[pos]
         else:
-            tiles[pos] = 1
+            tiles[pos] = None
     return tiles
 
 
 def second_part(tiles):
     def switch(tiles):
-        nei_tiles = defaultdict(int)
+        nei_tiles, n_tiles = defaultdict(int), {}
         for tile in tiles:
             for nei in dir_tup:
                 nei_tiles[tuple(map(sum, zip(tile, nei)))] += 1
-
-        n_tiles = {}
         for coord, neis in nei_tiles.items():
-            if coord in tiles:
-                if 0 < neis < 3:
-                    n_tiles[coord] = 1
-            elif coord not in tiles and neis == 2:
-                n_tiles[coord] = 1
+            if neis == 2:
+                n_tiles[coord] = None
+            elif coord in tiles and neis == 1:
+                n_tiles[coord] = None
         return n_tiles
 
     for _ in range(100):
@@ -43,7 +38,8 @@ def second_part(tiles):
 
 
 with open('2020/inputs/day24.txt', 'r') as inp:
-    tiles = first_part(inp.read().split('\n'))
+    tiles = first_part(sub('se', 'u', sub('sw', 'b', sub(
+            'ne', 'v', sub('nw', 'p', inp.read())))).split('\n'))
     std.write('Day 24\nFirst part: ')
     std.write(f'{len(tiles)}\nSecond part: ')
     std.write(f'{second_part(tiles)}\n')
