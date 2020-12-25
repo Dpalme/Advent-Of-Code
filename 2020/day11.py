56263c094a23b355cbd1ed839ff1ff8e25f35576
Dpalme <diegoPalmerin24@gmail.com>
2020-12-24 19:13:14 -0600

    dictionary optimization

--- 2020/day11.py
+++ 2020/day11.py
@@ -1,45 +1,55 @@
 from itertools import product
 from sys import stdout
+from collections import defaultdict
 
-DIRS = set(product((-1, 0, 1), repeat=2)).difference(set([(0, 0)]))
+
+def build_sts(lns):
+    sts = {}
+    for y, ln in enumerate(lns):
+        for x, seat in enumerate(ln):
+            if seat == 'L':
+                sts[(x, y)] = 0
+    return sts, len(lns[0]), len(lns)
 
 
-def setup(sts, line):
-    xm, ym, res = len(sts[0]), len(sts), None
+def setup(sts, xm, ym, line):
+    def cycle(sts):
+        def neighbors(crd):
+            for i, j in product((-1, 0, 1), repeat=2):
+                if i == j == 0:
+                    continue
+                dx, dy = crd[0] + i, crd[1] + j
+                while 0 <= dx < xm and 0 <= dy < ym:
+                    if (dx, dy) in sts or not line:
+                        yield dx, dy
+                        break
+                    dx, dy = dx + i, dy + j
 
-    def crowd(x, y, brkr):
-        crowded = 0
-        for i, j in DIRS:
-            dx, dy = x + i, y + j
-            while 0 <= dx < xm and 0 <= dy < ym:
-                if sts[dy][dx] != '.' or not line:
-                    crowded += sts[dy][dx] == '#'
-                    if brkr and crowded >= 1:
-                        return crowded
-                    break
-                dx, dy = dx + i, dy + j
-        return crowded
-
-    def cycle():
-        copied, counter = [[val for val in row] for row in sts], 0
-        for y in range(ym):
-            for x, seat in enumerate(sts[y]):
-                if seat != '.':
-                    crowded = crowd(x, y, seat == 'L')
-                    if seat == 'L' and crowded == 0:
-                        copied[y][x] = '#'
-                    elif seat == '#' and crowded > 3 + line:
-                        copied[y][x] = 'L'
-                    counter += copied[y][x] == '#'
-        return copied, counter
-
+        nei_tiles = defaultdict(int)
+        for tile, val in sts.items():
+            if val:
+                for nei in neighbors(tile):
+                    nei_tiles[nei] += 1
+        n_sts = {}
+        for crd, state in sts.items():
+            if state:
+                if nei_tiles[crd] > 3 + line:
+                    n_sts[crd] = 0
+                else:
+                    n_sts[crd] = state
+            elif nei_tiles[crd] == 0:
+                n_sts[crd] = 1
+            else:
+                n_sts[crd] = state
+        return n_sts
+    res = None
     while res != sts:
-        res, (sts, counter) = sts, cycle()
-    return counter
+        res, sts = sts, cycle(sts)
+    return sum(sts.values())
 
 
 with open('2020/inputs/day11.txt', 'r') as inp:
-    sts = [[st for st in ln] for ln in inp.read().split('\n')]
+    sts, xm, ym = build_sts(inp.read().split('\n'))
     stdout.write('Day 11\nFirst part: ')
-    stdout.write(f'{setup(sts, False)}\nSecond part: ')
-    stdout.write(f'{setup(sts, True)}\n')
+    stdout.write(f'{setup(sts.copy(), xm, ym, False)}\nSecond part: ')
+    stdout.write(f'{setup(sts, xm, ym, True)}\n')
