89048691a1ac723884b7f426244ebd5021fac226
Dpalme <diegoPalmerin24@gmail.com>
2021-12-26 03:24:52 -0600

    optimization

--- 2021/day20.py
+++ 2021/day20.py
@@ -14,29 +14,29 @@ def neigh(crd):
     return tuple((crd[0]+dx, crd[1]+dy) for dx, dy in DIRS)
 
 
-def enhance(enh, img, ext):
+def enhance(enh, img, ext, last_int):
+    interior = {n for crd in last_int for n in neigh(crd)}
     def enh_algorithm(crds):
-        return enh[int(''.join('1' if img[v] else '0' for v in crds),2)]
-
-    new_img = defaultdict(lambda:ext)
-    for crd in {n for crd in img for n in neigh(crd)}:
-        new_img[crd] = enh_algorithm(neigh(crd))
-    return new_img
+        return enh[int(''.join(ext if v not in last_int else (
+                        '1' if v in img else '0') for v in crds), 2)]
+    return {crd for crd in interior if enh_algorithm(neigh(crd))}, interior
 
 
 def iterate(enh, img, iterations):
+    last_ext = {n for crd in img for n in neigh(crd)}
     for i in range(iterations):
-        img = enhance(enh, img, enh[0] if not enh[0] or i%2==0 else enh[-1])
-    return sum(img.values())
+        ext = str(enh[0]) if not enh[0] or (i-1) % 2 == 0 else str(enh[-1])
+        img, last_ext = enhance(enh, img, ext, last_ext)
+    return len(img)
 
 
 with open('2021/inputs/day20.txt', 'r') as inp:
     lns = inp.read().rsplit('\n\n')
-    enh = tuple('#' == c for c in lns[0])
-    img = defaultdict(int)
+    enh = tuple(('#' == c) << 0 for c in lns[0])
+    img = set()
     for y, row in enumerate(lns[1].split('\n')):
         for x, v in enumerate(row):
             if v == '#':
-                img[(x, y)] = 1
+                img.add((x, y))
     stdout.write(f'Day 20\nFirst part: {iterate(enh, img, 2)}\n')
     stdout.write(f'Second part: {iterate(enh, img, 50)}\n')
